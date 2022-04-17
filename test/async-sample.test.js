afd55048ae465dd6b7105e1564e735f63be3a042
yxiong <yxiong@fnz.com>
2022-04-18 02:15:19 +0800

    async

---
+++ test/async-sample.test.js
@@ -0,0 +1,17 @@
+
+import * as user from './user'
+jest.mock('./request')
+
+
+// The assertion for a promise must be returned.
+it('works with promises', () => {
+    expect.assertions(1);
+    return user.getUserName(4).then(data => expect(data).toEqual('Mark'));
+});
+
+// async/await can be used.
+it('works with async/await', async () => {
+    expect.assertions(1);
+    const data = await user.getUserName(4);
+    expect(data).toEqual('Mark');
+});
